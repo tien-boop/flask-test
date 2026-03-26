f5fec487caaf2dbffe5fe904218ecbbd5e6f247b
tien-boop <thuy.tien.tran@edu.huddinge.se>
2026-03-26 10:01:00 +0000

    next fix html

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Skapa en Flask-app
 app = Flask(__name__)
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", username=name)
 
 # Ange vad som händer när man går in på startsidan '/'
 @app.route('/')
 def home():
     return 'Hello world'
+
+# På adressen /hello/ kan man ange sitt namn som parameter
+@app.route("/<namn>")
+def hello(namn):
+    return "Hej " + namn
 
 # Starta webbappen när man kör python-filen
 if __name__ == "__main__":
